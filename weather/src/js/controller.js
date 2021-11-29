c912966d6b9b55b719f7e34a925e0591bffb0381
PastranaDigital <53458320+PastranaDigital@users.noreply.github.com>
2021-11-29 08:02:42 -0600

    base & base for weather

---
+++ weather/src/js/controller.js
@@ -0,0 +1,41 @@
+// fetch('https://accuweatherstefan-skliarovv1.p.rapidapi.com/getCurrentConditionsForTopCities', {
+// 	method: 'POST',
+// 	headers: {
+// 		'content-type': 'application/x-www-form-urlencoded',
+// 		'x-rapidapi-host': 'AccuWeatherstefan-skliarovV1.p.rapidapi.com',
+// 		'x-rapidapi-key': 'SIGN-UP-FOR-KEY',
+// 	},
+// 	body: {
+// 		group: '<REQUIRED>',
+// 		apiKey: '<REQUIRED>',
+// 	},
+// })
+// 	.then((response) => {
+// 		console.log(response);
+// 	})
+// 	.catch((err) => {
+// 		console.error(err);
+// 	});
+
+//? Input the degrees (number only)
+
+//? Image is rendered based on temperature
+
+//? 80+
+// shirt & shorts
+//? 79-70
+//
+//? 69-60
+//
+//? 59-55
+//
+//? 54-50
+//
+//? 49-45
+//
+//? 44-40
+//
+//? 39-33
+//
+//? 32-
+//
