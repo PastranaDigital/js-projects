922f23af9a14735bf87f95104ebf4d25c4f8a60d
PastranaDigital <53458320+PastranaDigital@users.noreply.github.com>
2022-10-04 11:51:54 -0500

    basic layout

---
+++ code-review/src/js/model.js
@@ -0,0 +1,21 @@
+export const state = [
+	{
+		type: 'Admin',
+		reviewers: [
+			'Katy Lorenti',
+			'Simone Yang',
+			'Jai Yarlagadda',
+			'Matt Sherman',
+			'Arkeith Cunningham',
+			'Oladayo Olarinde',
+		],
+	},
+	{
+		type: 'Apex',
+		reviewers: ['Jeff Watson', 'Patrick Skamarak', 'Kat Galang', 'Sal Carerra', 'Jai Yarlagadda', 'Matt Sherman'],
+	},
+	{
+		type: 'JavaScript',
+		reviewers: ['Omar Pastrana', 'Patrick Skamarak', 'Kat Galang', 'Sal Carerra', 'Jai Yarlagadda'],
+	},
+];
